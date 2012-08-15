0e779e7597e115be1c88f1748aabe0bcbd2cfe6e
Joao Cicero <jc_ifsp@hotmail.com>
2012-08-14 21:29:16 -0300

    tamanho da letra

--- ProjetoTeste/Form1.Designer.cs
+++ ProjetoTeste/Form1.Designer.cs
@@ -53,9 +53,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(78, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.Size = new System.Drawing.Size(149, 18);
             this.label1.TabIndex = 2;
             this.label1.Text = "Digite os Produtos";
             // 
